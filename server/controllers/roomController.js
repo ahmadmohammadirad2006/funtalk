1b2b94592f9f3dea2e10e7b665f9aec92db66efc
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-08-20 10:56:23 +0330

    feat: add messages virtual populate to room doc (server)

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -5,7 +5,10 @@ exports.deleteRoom = factory.deleteOne(Room);
 exports.updateRoom = factory.updateOne(Room);
 exports.createRoom = factory.createOne(Room);
 exports.getAllRooms = factory.getAll(Room);
-exports.getRoom = factory.getOne(Room);
+exports.getRoom = factory.getOne(Room, {
+  path: 'messages',
+  select: '-room',
+});
 
 // this unables user to set the createdAt field and sets createdBy field to the current user's id
 exports.filterBody = (req, res, next) => {
--- server/models/roomModel.js
+++ server/models/roomModel.js
@@ -15,32 +15,44 @@ const isOneEmoji = function (emoji) {
   }
 };
 
-const roomSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A room must have a name'],
-    unique: true,
-    minlength: [4, 'Room name must be atleast 4 characters'],
-    maxlength: [12, "Room name mustn't be more than 12 characters"],
-  },
-  description: {
-    type: String,
-    required: [true, 'A room must have a description'],
-    minlength: [30, 'Description must be atleast 30 characters'],
-    maxlength: [90, 'Description must not be more than 90 characters'],
-  },
-  emoji: {
-    type: String,
-    required: [true, 'A room must have a emoji'],
-    validate: {
-      validator: isOneEmoji,
-      message: 'Please provide exactly one emoji',
+const roomSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A room must have a name'],
+      unique: true,
+      minlength: [4, 'Room name must be atleast 4 characters'],
+      maxlength: [12, "Room name mustn't be more than 12 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, 'A room must have a description'],
+      minlength: [30, 'Description must be atleast 30 characters'],
+      maxlength: [90, 'Description must not be more than 90 characters'],
+    },
+    emoji: {
+      type: String,
+      required: [true, 'A room must have a emoji'],
+      validate: {
+        validator: isOneEmoji,
+        message: 'Please provide exactly one emoji',
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
     },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+roomSchema.virtual('messages', {
+  ref: 'Message',
+  localField: '_id',
+  foreignField: 'room',
 });
 
 const Room = mongoose.model('Room', roomSchema);
