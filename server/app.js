e0aa8475058d0526978a91b81a3ac8d2e89acd56
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-09-02 11:29:18 +0330

    feat: add build command

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const app = express();
 // Limit requests per hour for each IP
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 hour).
+  limit: 200, // Limit each IP to 100 requests per `window` (here, per 1 hour).
   standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
